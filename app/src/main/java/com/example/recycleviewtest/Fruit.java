fce745d43e2601130a79b993e94b703f5348097b
Nancy-627 <w2433798615@qq.com>
2020-10-12 08:06:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewtest/Fruit.java
@@ -0,0 +1,19 @@
+package com.example.recycleviewtest;
+
+public class Fruit {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    private int imageId;
+    public Fruit(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+}
